3b55f3efc885bce991399ddef3a40fcdd6de29a4
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-05-09 05:24:19 -0400

    Simplification

--- crates/ventana/src/backend.rs
+++ crates/ventana/src/backend.rs
@@ -1,9 +1,9 @@
 #[cfg(windows_platform)]
 pub use ventana_backend_win32 as win32;
 #[cfg(windows_platform)]
-pub use win32::Win32 as Backend;
+pub use win32::Win32 as Win32;
 
 #[cfg(wayland_platform)]
 pub use ventana_backend_wayland as wayland;
 #[cfg(wayland_platform)]
-pub use wayland::Wayland as Backend;
+pub use wayland::Wayland as Wayland;
