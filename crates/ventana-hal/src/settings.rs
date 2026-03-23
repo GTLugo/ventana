8fee42198d894f0e0ab297883f0a02f6b2c2e98a
Gabriel Lugo-Maldonado <g.alexander.lugo@gmail.com>
2026-03-23 02:30:10 -0400

    Removed obsolete defaults for WindowSettings

--- crates/ventana-hal/src/settings.rs
+++ crates/ventana-hal/src/settings.rs
@@ -15,15 +15,4 @@ pub struct WindowSettings {
   pub visibility: Visibility,
 }
 
-impl Default for WindowSettings {
-  fn default() -> Self {
-    Self {
-      title: SmolStr::from("Window"),
-      size: Size::Logical((800.0, 500.0).into()),
-      position: None,
-      visibility: Default::default(),
-    }
-  }
-}
-
 impl WindowSettings {}
