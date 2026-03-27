586c3ca6f310fead9bd4d10ecb9914e0f5e929e4
Gabriel Lugo-Maldonado <g.alexander.lugo@gmail.com>
2026-03-27 10:30:23 -0400

    Fix cargo manifests

--- crates/ventana/src/backend.rs
+++ crates/ventana/src/backend.rs
@@ -1,7 +1,7 @@
 #[cfg(wayland_platform)]
-pub use ventana_backend_wayland as wayland;
+pub use backend_wayland as wayland;
 #[cfg(windows_platform)]
-pub use ventana_backend_win32 as win32;
+pub use backend_win32 as win32;
 #[cfg(wayland_platform)]
 pub use wayland::Wayland;
 #[cfg(windows_platform)]
